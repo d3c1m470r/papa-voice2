7a253c759c0f3fbff8cf19b3d57a89c251f2d03f
d3c1m470r <132501689+d3c1m470r@users.noreply.github.com>
2025-07-10 20:46:25 +0200

    more hotkey misery5

--- papaAddon/buildVars.py
+++ papaAddon/buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	"addon_description": _("Reads article content aloud, skipping ads and menus."),
 	# version
-	"addon_version": "1.1.5",
+	"addon_version": "1.1.6",
 	# Author(s)
 	"addon_author": "fagyhalal",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2023.1",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.4",
+	"addon_lastTestedNVDAVersion": "2025.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
